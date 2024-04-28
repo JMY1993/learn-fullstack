1211b97a86dc2ff7be6b68483aaa043dab74e2b5
JMY1993 <yangmohan1993@163.com>
2024-04-29 01:44:30 +0800

    configured the base dir to be relative

--- part3/simpleHttpServer/index.js
+++ part3/simpleHttpServer/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const notes = require("./db.json");
 
+app.use(express.json());
+
 app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
@@ -15,6 +17,20 @@ app.get("/api/notes/:id", (req, res) => {
     res.status(404).end();
   }
 });
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(
+    `Deleted note: ${notes.notes.find((note) => note.id === id).content}.`
+  );
+  res.status(204).end();
+});
 const PORT = 3001;
+
+app.post("/api/notes", (req, res) => {
+  const note = req.body;
+  console.log(note);
+  res.status(404).end();
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
